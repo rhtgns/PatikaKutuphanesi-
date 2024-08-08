d81cc926520cfa9a99e2ee1f4612033bbc18f4a5
Rohat Güneş <rohatgns44@gmail.com>
2024-08-08 15:53:54 +1000

    Add project files.

---
+++ PatikaKutuphanesi,/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaKutuphanesi_
+{
+    public class Kitap
+    {
+        public string Ad {  get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi {  get; set; }
+        public int SayfaSayisi {  get; set; }
+        public string YayinEvi {  get; set; }
+        public  DateTime KayitTarihi { get; set; }
+
+
+        public Kitap()//burada default constuctor yazıyoruz 
+        {
+           
+
+
+            KayitTarihi = DateTime.Now;//kayıt tarihi o an gercek zamanlı olarak atanıyor 
+
+        }
+        // şimdi ise  paremeter alan constructor yapacagız
+        //constructor ise bir yapıcı fonksiyondur burda degerleir alır ve propertylere atar
+
+        public  Kitap(string ad,string yazarAdi,string yazarSoyadi,int sayfaSayisi,string yayinEvi)
+        {
+            Ad= ad;
+            YazarAdi = yazarAdi;
+            YazarSoyadi = yazarSoyadi;
+            SayfaSayisi = sayfaSayisi;
+            YayinEvi = yayinEvi;
+            KayitTarihi= DateTime.Now;//  we create this with real time 
+
+
+        }
+
+
+    }
+}
---
+++ PatikaKutuphanesi,/Program.cs
@@ -0,0 +1,47 @@
+﻿using PatikaKutuphanesi_;
+using System;
+using System.Net.WebSockets;
+
+
+public class Program  
+    //kitapları temsil eden bir sınıftır ve kitapları ait bilgileri içerir 
+{
+
+    public static void Main()
+    {
+
+        Kitap kitap1 = new Kitap();//we create the object //new burda yeni bir nesne oluşturmak için kullanılır 
+        kitap1.Ad = "Amok Koşucusu";
+        kitap1.YazarAdi = "Stefan ";
+        kitap1.YazarSoyadi = "Zweig";
+        kitap1.SayfaSayisi = 60;
+        kitap1.YayinEvi = "İs Bankası Yayın Evi";
+
+        Kitap kitap2 = new Kitap("Adı: Satranc", "Stefan", "Zweig",60, "İs Bankası Yayın Evi");
+
+        //we shows books info on the screen 
+        Console.WriteLine("kitap 1:");
+        Console.WriteLine($"Ad:{kitap1.Ad}");
+        Console.WriteLine($"YazarAdı :{kitap1.YazarAdi}");
+        Console.WriteLine($"YazarSoyadi:{kitap1.YazarSoyadi}");
+        Console.WriteLine($"Sayfasayisi{kitap1.SayfaSayisi}");
+        Console.WriteLine($"YayinEvi:{kitap1.YayinEvi}");
+        Console.WriteLine($"KayıtTarihi:{kitap1.KayitTarihi}");
+
+
+
+
+        Console.WriteLine("\nkitap 2:");
+        Console.WriteLine($"Ad:{kitap2.Ad}");
+        Console.WriteLine($"YazarAdı :{kitap2.YazarAdi}");
+        Console.WriteLine($"YazarSoyadi:{kitap2.YazarSoyadi}");
+        Console.WriteLine($"Sayfasayisi{kitap2.SayfaSayisi}");
+        Console.WriteLine($"YayinEvi:{kitap2.YayinEvi}");
+        Console.WriteLine($"KayıtTarihi{kitap2.KayitTarihi}");
+
+
+
+    }
+
+}
+
